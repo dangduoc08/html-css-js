1f7173b757e59319e9a1d2b6db1db3d5d5ce9551
Đăng Được <duoc200894@gmail.com>
2021-06-29 05:11:28 +0700

    Add race chart

--- public/chart.js
+++ public/chart.js
@@ -2,56 +2,93 @@
 
 (function () {
   const pivot = 50000
+  const animationDuration = pivot / 10000
   const races = [
     {
-      company: 'apple',
-      value: 234241
-    },
-    {
       company: 'google',
-      value: 167713
+      value: 167713,
+      bg: '#95afca'
     },
     {
       company: 'samsung',
-      value: 61098
+      value: 61098,
+      bg: '#ed9a9b'
     },
     {
       company: 'amazon',
-      value: 125263
+      value: 125263,
+      bg: '#add4d1'
+    },
+    {
+      company: 'apple',
+      value: 234241,
+      bg: '#d6d0cd'
     },
     {
       company: 'microsoft',
-      value: 108847
+      value: 108847,
+      bg: '#9bc795'
     }
   ]
 
+  function addIncreaseFrame(name, width) {
+    const style = document.createElement('style')
+    style.innerHTML = `
+      @keyframes increase_frame_${name} {
+        from {
+          width: 0%;
+        }
+        to {
+          width: ${width};
+        }
+      }
+    `
+    document.getElementsByTagName('head')[0].appendChild(style)
+
+    return `increase_frame_${name}`
+  }
+
   const techCompanyAxis = document.getElementById('tech-company-axis')
   let chartCompanyHTML = ''
+  let chartPivotHTML = ''
+  races.sort((a, b) => b.value - a.value)
+  let totalColumns = Math.ceil(races[0].value / pivot)
+
   races.forEach((race, index) => {
     const value = race.value
-    const totalChartVal = Math.round(value / pivot)
+    const totalChartVal = Math.ceil(value / pivot)
+
     let chartValues = ``
     for (let i = 1; i <= totalChartVal; i++) {
       const isLast = i === totalChartVal
-      let width = '100%'
-      if (value < pivot) {
-        width = value * 100 / pivot + '%'
-      } else if (isLast) {
-        width = (value - (pivot * i)) / pivot + '%'
+      const animationDelay = (i - 1) * animationDuration
+
+      if (index === 0 && !isLast) {
+        chartPivotHTML += `<div>${i * pivot}</div>`
       }
 
-      chartValues += `<div class='chart__value' style='width: ${width}'>
-          <div>${!isLast ? '' : race.company[0].toUpperCase() + race.company.slice(1)}</div>
-          <div>${!isLast ?
-          index === 0 ?
-            pivot * i :
-            '' :
-          value
-        }</div>
+      let width = '100%'
+      const remain = value - (pivot * i)
+      if (remain < 0) {
+        width = (value - ((i - 1) * pivot)) * 100 / pivot + '%'
+      }
+
+      let animationName = 'increase_frame'
+      if (isLast) {
+        animationName = addIncreaseFrame(race.company, width)
+      }
+
+
+
+      chartValues += `<div class='chart__value' style='background-color: ${race.bg}; animation-name: ${animationName}; animation-duration: ${animationDuration}s; animation-delay: ${animationDelay}s'>
+          ${isLast ?
+          `<div>${race.company[0].toUpperCase() + race.company.slice(1)}</div><div>${value}</div>` :
+          ''}
         </div>`
     }
 
-    chartCompanyHTML += `<div class='chart__company' id=${race.company}>${chartValues}</div>`
+    chartCompanyHTML += `<div class='chart__company' id=${race.company} style='grid-template-columns: repeat(${totalColumns}, 1fr);'>${chartValues}</div>`
   })
-  techCompanyAxis.innerHTML = chartCompanyHTML
+
+  techCompanyAxis.innerHTML = `<div class='chart__pivot' style='grid-template-columns: repeat(${totalColumns}, 1fr);'>` + chartPivotHTML + '</div>' + chartCompanyHTML
 })()
